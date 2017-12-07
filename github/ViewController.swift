c2ee6c976cdaeadabd20918e43382ff1a401e5e0
Gennaro Artiaco <gennaroartiaco@10-20-49-24.0200.fed.it.iosda.org>
2017-12-07 17:09:13 +0100

    Initial Commit

---
+++ github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github
+//
+//  Created by Gennaro Artiaco on 07/12/17.
+//  Copyright © 2017 Gennaro Artiaco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
